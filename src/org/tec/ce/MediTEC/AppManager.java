24b4e298b8be69942c028a8dad8b5c995cad2a01
Sebastián Mora <sebas.smr@hotmail.com>
2017-05-04 18:42:35 -0600

    Update AppManager.java

--- src/org/tec/ce/MediTEC/AppManager.java
+++ src/org/tec/ce/MediTEC/AppManager.java
@@ -4,6 +4,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.tec.ce.MediTEC.resources.Doctors;
 
 public class AppManager extends ResourceConfig {
+	//hola
 	public AppManager(){
 		register(Doctors.class);
 		System.out.print("hola");
