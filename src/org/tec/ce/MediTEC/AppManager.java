c36b48dee5ad93f695fff10308e7a64dc2d7dfa7
sebas97012 <sebas.smr@hotmail.com>
2017-05-04 19:03:43 -0600

    Second Commit

--- src/org/tec/ce/MediTEC/AppManager.java
+++ src/org/tec/ce/MediTEC/AppManager.java
@@ -4,10 +4,9 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.tec.ce.MediTEC.resources.Doctors;
 
 public class AppManager extends ResourceConfig {
-	//hola
 	public AppManager(){
 		register(Doctors.class);
 		System.out.print("hola");
 	}
 
-}
+}//probando
