6435529d32caebcfc7d8d5501846baf505fa4716
sebas97012 <sebas.smr@hotmail.com>
2017-05-06 01:56:44 -0600

    JSON parser implemented

--- src/org/tec/ce/MediTEC/resources/Doctors.java
+++ src/org/tec/ce/MediTEC/resources/Doctors.java
@@ -4,14 +4,33 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 
 @Path("/docs")
 public class Doctors {
-
+	
+ private List<String> doctors = new ArrayList<>();
+ 
+ public Doctors(){
+	 this.doctors.add("Sebastián");
+	 this.doctors.add("Paz");
+	 this.doctors.add("Arturo");
+	 this.doctors.add("Nayib");
+	 this.doctors.add("Adrian");
+ }
+ 
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getDoctors(){
-		return Response.ok().entity("Sebas").build();
-	}
+		if (this.doctors.isEmpty()){
+			return Response.noContent().build();
+			}else{
+				return Response.ok().entity(doctors).build();
+			}
+		}
+		
 }
