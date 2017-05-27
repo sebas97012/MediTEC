ea87ff3460dceb48dfd5dab08b8c01ebdbc459b0
Crusher11 <paz.hs@outlook.com>
2017-05-27 02:53:11 -0600

    Se añadió el chat entre doctores

---
+++ src/org/tec/ce/MediTEC/dto/MessageDTO.java
@@ -0,0 +1,33 @@
+package org.tec.ce.MediTEC.dto;
+
+
+public class MessageDTO implements Comparable<MessageDTO> {
+	
+	private String message;
+	private int id;
+
+	
+	public MessageDTO (String message, int id){
+		this.message = message;
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public int compareTo(MessageDTO arg0) {
+		if(this.id > arg0.getId()){
+			return 1;
+		} else if(this.id < arg0.getId()){
+			return -1;
+		} else{
+			return 0;
+		}
+	}
+}
---
+++ src/org/tec/ce/MediTEC/resources/Chat.java
@@ -0,0 +1,34 @@
+package org.tec.ce.MediTEC.resources;
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.tec.ce.DataStructures.LinkedList.LinkedList;
+import org.tec.ce.MediTEC.dto.MessageDTO;
+
+@Path("/chat")
+public class Chat {
+	private static LinkedList<MessageDTO> chat = new LinkedList<MessageDTO>();
+	 
+	public Chat(){
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getChat(){
+		return Response.ok().entity(getChat()).build();
+	}
+	
+	@PUT 
+	@Consumes (MediaType.APPLICATION_JSON)
+	public Response insertMessage(MessageDTO message){
+		chat.insertAtEnd(message);
+		return Response.ok().build();
+	}
+}
