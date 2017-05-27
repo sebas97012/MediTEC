0155baf2da3c2c3e3ddfea9466fa4b077bebe38e
Arturo Córdoba <arturocv16@gmail.com>
2017-05-26 18:56:23 -0600

    Implementado envio de correos

--- src/org/tec/ce/MediTEC/EmailSenderService.java
+++ src/org/tec/ce/MediTEC/EmailSenderService.java
@@ -1,5 +1,48 @@
 package org.tec.ce.MediTEC;
 
-public class EmailSenderService {
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-}
+public class EmailSenderService {
+    private static String correoRemitente = "meditec.webservice@gmail.com";
+    private static String password = "wkgospp3";
+
+    public static void sendEmail(String correoReceptor, String subject, String mensaje) {
+        try {
+            Properties props = new Properties();
+            props.setProperty("mail.smtp.host", "smtp.gmail.com"); //Nombre del host del correo
+            props.setProperty("mail.smtp.starttls.enable", "true"); //TLS
+            props.setProperty("mail.smtp.port", "587"); // Puerto de gmail para envio de correos
+            props.setProperty("mail.smtp.user", correoRemitente); // Nombre del usuario
+            props.setProperty("mail.smtp.auth", "true");
+            Session session = Session.getDefaultInstance(props);
+            session.setDebug(true); // QUITAR
+
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(correoRemitente)); //Quien envia el correo
+
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(correoReceptor)); //A quien va dirigido
+
+            message.setSubject(subject); //Se pone el subject del correo
+
+            message.setText(mensaje); //Se pone el mensaje a enviar
+
+            Transport t = session.getTransport("smtp");
+
+            t.connect(correoRemitente, password);
+
+            t.sendMessage(message, message.getAllRecipients()); //Se envia el mensaje
+
+            t.close(); //Se cierra la conexion
+
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
